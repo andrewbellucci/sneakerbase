e2833860b94f0e26a063b769d349322ac0c97922
Andrew Bellucci <abellucci@bellu.dev>
2023-03-22 02:14:38 -0400

    status

--- apps/api-gateway/src/routes/health.ts
+++ apps/api-gateway/src/routes/health.ts
@@ -6,7 +6,7 @@ export default async function (fastify: FastifyInstance) {
     async (request, reply) => {
       try {
         await prisma.$queryRaw`SELECT 1`;
-        reply.status(204);
+        reply.status(200);
       } catch {
         reply.status(500);
       }
