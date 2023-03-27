c0c15f273492049089432d79a10ce457b340f614
Andrew Bellucci <abellucci@bellu.dev>
2023-03-27 16:35:46 -0400

    up

--- apps/jobs/src/tasks/process-prices.ts
+++ apps/jobs/src/tasks/process-prices.ts
@@ -1,10 +1,10 @@
 import { logger } from "@sneakerbase/utils";
 import { processAllSneakerPrices } from "../lib/prices";
 import schedule from "node-schedule";
-import { EVERY_SUNDAY_AT_MIDNIGHT } from "../utils/cron";
+import { EVERY_MONDAY_AT_MIDNIGHT } from "../utils/cron";
 
 export default async function () {
-  schedule.scheduleJob(EVERY_SUNDAY_AT_MIDNIGHT, async function () {
+  schedule.scheduleJob(EVERY_MONDAY_AT_MIDNIGHT, async function () {
     const start = Date.now();
     logger.info("Starting Processing of all Sneaker Prices");
 
--- apps/jobs/src/utils/cron.ts
+++ apps/jobs/src/utils/cron.ts
@@ -2,3 +2,4 @@ export const EVERY_DAY_AT_5AM = "0 05 * * *" as const;
 export const EVERY_DAY_AT_5PM = "0 17 * * *" as const;
 export const EVERY_DAY_AT_MIDNIGHT = "0 0 * * *" as const;
 export const EVERY_SUNDAY_AT_MIDNIGHT = "0 0 * * 0" as const;
+export const EVERY_MONDAY_AT_MIDNIGHT = "0 0 * * 1" as const;
