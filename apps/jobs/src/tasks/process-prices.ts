8ec6b891f2991c60f570d0775385723fe077fa76
Andrew Bellucci <abellucci@bellu.dev>
2023-04-14 12:41:19 -0400

    inclusion of everything

--- apps/jobs/src/tasks/process-prices.ts
+++ apps/jobs/src/tasks/process-prices.ts
@@ -1,10 +1,10 @@
 import { logger } from "@sneakerbase/utils";
 import { processAllSneakerPrices } from "../lib/prices";
 import schedule from "node-schedule";
-import { EVERY_FRIDAY_AT_4_15PM } from "../utils/cron";
+import { EVERY_FRIDAY_AT_4_45PM } from "../utils/cron";
 
 export default async function () {
-  schedule.scheduleJob(EVERY_FRIDAY_AT_4_15PM, async function () {
+  schedule.scheduleJob(EVERY_FRIDAY_AT_4_45PM, async function () {
     const start = Date.now();
     logger.info("Starting Processing of all Sneaker Prices");
 
