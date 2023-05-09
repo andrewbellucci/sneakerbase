d1a441dd1ce52a4efb0bed30b3b8e3c6e34a6d21
Andrew Bellucci <abellucci@bellu.dev>
2023-05-09 14:49:48 -0400

    top-rated

--- apps/api-gateway/src/routes/web-data.ts
+++ apps/api-gateway/src/routes/web-data.ts
@@ -39,6 +39,41 @@ export default async function (fastify: FastifyInstance) {
     }
   );
 
+  // Grabs all the most upvoted products in our catalog
+  fastify.withTypeProvider<ZodTypeProvider>().get('/top-rated',
+    async (request, reply) => {
+      if (env.WEB_TOKEN !== request.headers.authorization) {
+        reply.status(401);
+        return;
+      }
+
+      try {
+        const productVisitCounts = await prisma.upVote.groupBy({
+          by: ['productId'],
+          _count: {
+            productId: true
+          },
+          orderBy: { _count: { productId: 'desc' } },
+          take: 5
+        });
+
+        const products = await prisma.product.findMany({
+          where: {
+            id: {
+              in: productVisitCounts.map(p => p.productId)
+            }
+          },
+        });
+
+        reply.status(200);
+
+        return products;
+      } catch {
+        reply.status(500);
+      }
+    }
+  );
+
   // Gets the product of the day
   fastify.withTypeProvider<ZodTypeProvider>().get('/product-of-the-day',
     async (request, reply) => {
