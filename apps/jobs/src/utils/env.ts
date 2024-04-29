a800aeb3c53bf1ae5976a35563c7133cc0922ab6
andrewbellucci <andrew@bellu.dev>
2024-04-29 14:57:35 -0400

    .

--- apps/jobs/src/utils/env.ts
+++ apps/jobs/src/utils/env.ts
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-import z from 'zod';
+import dotenv from "dotenv";
+import z from "zod";
 
 dotenv.config();
 
@@ -7,8 +7,8 @@ const EnvSchema = z.object({
   DATABASE_URL: z.string(),
   STOCKX_ALGOLIA_APP_ID: z.string(),
   STOCKX_ALGOLIA_API_KEY: z.string(),
-  SPACES_ACCESS_KEY: z.string(),
-  SPACES_SECRET_KEY: z.string(),
+  S3_ACCESS_KEY: z.string(),
+  S3_SECRET_KEY: z.string(),
   PROXY_HOST: z.string(),
   PROXY_PORT: z.string(),
   PROXY_USERNAME: z.string(),
@@ -16,12 +16,14 @@ const EnvSchema = z.object({
   SENTRY_DSN: z.string(),
 });
 
+// TODO: update this to use cloudflare R2
+
 const variables = {
   DATABASE_URL: process.env.DATABASE_URL,
   STOCKX_ALGOLIA_APP_ID: process.env.STOCKX_ALGOLIA_APP_ID,
   STOCKX_ALGOLIA_API_KEY: process.env.STOCKX_ALGOLIA_API_KEY,
-  SPACES_ACCESS_KEY: process.env.SPACES_ACCESS_KEY,
-  SPACES_SECRET_KEY: process.env.SPACES_SECRET_KEY,
+  S3_ACCESS_KEY: process.env.SPACES_ACCESS_KEY,
+  S3_SECRET_KEY: process.env.SPACES_SECRET_KEY,
   PROXY_HOST: process.env.PROXY_HOST,
   PROXY_PORT: process.env.PROXY_PORT,
   PROXY_USERNAME: process.env.PROXY_USERNAME,
--- apps/jobs/src/utils/storage.ts
+++ apps/jobs/src/utils/storage.ts
@@ -1,53 +1,53 @@
-import { DeleteObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import { env } from "./env";
 
-const spaceName = 'sneakerbase';
+const spaceName = "sneakerbase";
 const s3 = new S3Client({
-  endpoint: 'https://990780148d62a437b13e976156284680.r2.cloudflarestorage.com',
+  endpoint: "https://990780148d62a437b13e976156284680.r2.cloudflarestorage.com",
   credentials: {
-    accessKeyId: env.SPACES_ACCESS_KEY,
-    secretAccessKey: env.SPACES_SECRET_KEY,
+    accessKeyId: env.S3_ACCESS_KEY,
+    secretAccessKey: env.S3_SECRET_KEY,
   },
-  region: 'auto',
+  region: "auto",
 });
 
-export async function removeFile(
-  fileName: string,
-  bucketName: string,
-) {
-  return await removeS3(
-    spaceName,
-    bucketName + '/' + fileName
-  );
+export async function removeFile(fileName: string, bucketName: string) {
+  return await removeS3(spaceName, bucketName + "/" + fileName);
 }
 
 export function uploadS3(
   file: Buffer,
   bucket: string,
   name: string,
-  contentType?: string,
+  contentType?: string
 ) {
   const params = contentType
     ? {
-      ACL: 'public-read',
-      Bucket: bucket,
-      Key: String(name),
-      Body: file,
-      ContentType: contentType,
-    }
+        ACL: "public-read",
+        Bucket: bucket,
+        Key: String(name),
+        Body: file,
+        ContentType: contentType,
+      }
     : {
-      ACL: 'public-read',
-      Bucket: bucket,
-      Key: String(name),
-      Body: file,
-    };
+        ACL: "public-read",
+        Bucket: bucket,
+        Key: String(name),
+        Body: file,
+      };
 
   return s3.send(new PutObjectCommand(params));
 }
 
 function removeS3(bucket: string, name: string) {
-  return s3.send(new DeleteObjectCommand({
-    Bucket: bucket,
-    Key: String(name),
-  }));
+  return s3.send(
+    new DeleteObjectCommand({
+      Bucket: bucket,
+      Key: String(name),
+    })
+  );
 }
