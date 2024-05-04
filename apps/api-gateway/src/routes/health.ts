02a38975023eb2d360792248cdb4edf04812fd4e
andrewbellucci <andrew@bellu.dev>
2024-05-03 21:23:29 -0400

    .

--- apps/api-gateway/src/routes/health.ts
+++ apps/api-gateway/src/routes/health.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../utils/prisma";
+import {logger} from "@sneakerbase/utils";
 
 export default async function (fastify: FastifyInstance) {
   fastify.get('/',
@@ -7,7 +8,8 @@ export default async function (fastify: FastifyInstance) {
       try {
         await prisma.$queryRaw`SELECT 1`;
         reply.status(200);
-      } catch {
+      } catch (error) {
+        logger.error(error);
         reply.status(500);
       }
     }
