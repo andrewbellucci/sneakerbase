1a0e56df47a932f08331f0b45cf0c6f4878963c7
andrewbellucci <andrew@bellu.dev>
2024-08-01 17:38:21 -0400

    .

--- apps/website/app/root.tsx
+++ apps/website/app/root.tsx
@@ -20,6 +20,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JGDTDM3B39"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JGDTDM3B39');        
+        `}}/>
       </head>
       <body className="bg-off-white min-h-screen">
         {children}
